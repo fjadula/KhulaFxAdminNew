0be79089f903844e0ffd55860114cb273d862404
fjadula <125087186+fjadula@users.noreply.github.com>
2025-11-08 13:33:57 +0200

    dsfd

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7222/api',
+  //apiUrl: 'https://localhost:7222/api',
+  apiUrl: 'https://khulafx.com/api',
   googleClientId: '292402212243-0c8vsht1uimcbue4shpdk0hiofbefcu7.apps.googleusercontent.com'
 };

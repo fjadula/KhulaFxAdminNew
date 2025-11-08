fcd0a0a85e05ce3c1b15840ff052fb437bd37050
fjadula <125087186+fjadula@users.noreply.github.com>
2025-11-08 17:16:55 +0200

    Prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://khulafx.com/api',
-  googleClientId: '824568194202-imjjr1ovaa97k9b6pj761e0b1jullu6l.apps.googleusercontent.com'
+  googleClientId: '0c8vsht1uimcbue4shpdk0hiofbefcu7.apps.googleusercontent.com'
 };

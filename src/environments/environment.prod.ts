c7220e61fc70864fca187b3f85efd1b7375366cc
fjadula <125087186+fjadula@users.noreply.github.com>
2025-11-08 17:25:47 +0200

    Clientid

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://khulafx.com/api',
-  googleClientId: '0c8vsht1uimcbue4shpdk0hiofbefcu7.apps.googleusercontent.com'
+  googleClientId: '292402212243-0c8vsht1uimcbue4shpdk0hiofbefcu7.apps.googleusercontent.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'https://localhost:7222/api',
-  googleClientId: '0c8vsht1uimcbue4shpdk0hiofbefcu7.apps.googleusercontent.com'
+  googleClientId: '292402212243-0c8vsht1uimcbue4shpdk0hiofbefcu7.apps.googleusercontent.com'
 };
